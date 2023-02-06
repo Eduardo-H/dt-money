70009f2f8903003f5a6f5a29375f2d04bffc8dd3
Eduardo-H <edu-henrique2014@hotmail.com>
2023-02-05 21:49:57 -0300

    Transactions table and search form implemented

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,16 @@
+import { MagnifyingGlass } from 'phosphor-react';
+
+import { SearchFormContainer } from './styles';
+
+export function SearchForm() {
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Search transactions" />
+
+      <button>
+        <MagnifyingGlass size={20} />
+        Search
+      </button>
+    </SearchFormContainer>
+  )
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,53 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
+
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Salary</td>
+              <td>
+                <PriceHighlight variant="income">
+                  $3,000.00
+                </PriceHighlight>
+              </td>
+              <td>Jobs</td>
+              <td>07/02/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Groceries</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - $100.00
+                </PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>05/02/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Freelance</td>
+              <td>
+                <PriceHighlight variant="income">
+                  $1,000.00
+                </PriceHighlight>
+              </td>
+              <td>Jobs</td>
+              <td>02/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
